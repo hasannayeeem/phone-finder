bdb54d36b51c21162019c8fa9ff5db3fe1684f33
hasannayeeem <hasannayeeem@gmail.com>
2022-03-02 23:40:36 +0600

    final

--- js/app.js
+++ js/app.js
@@ -14,27 +14,25 @@ const displaySearchResult = phones => {
     // console.log(phones);
     const searchResult = document.getElementById('search-result');
     phones.forEach(phone => {
-        // console.log(phone);
         const div = document.createElement('div');
         div.classList.add('col');
         div.innerHTML = `
         <div class="card h-100 border-0 shadow">
-            <img src="${phone.image}" class="card-img-top" alt="...">
+            <img src="${phone.image}" class="card-img-top w-50 mx-auto" alt="...">
             <div class="card-body">
                <h5 class="card-title">${phone.phone_name}</h5>
                <p class="card-text">brand: ${phone.brand}</p>
-               <button class="btn__link" onclick="loadPhonesByName('${phone.slug}')">details</button>
+               <button class="btn__link border-0 btn-success shadow-none" onclick="loadPhonesByName('${phone.slug}')">details</button>
             </div>
         </div>
         `;
+        
         searchResult.appendChild(div);
     })
 }
 
 const loadPhonesByName = phoneId => {
-    // console.log(phoneId);
     const url = `https://openapi.programming-hero.com/api/phone/${phoneId}`;
-    // console.log(url);
     fetch(url)
     .then(res => res.json())
     .then(data => displayPhoneDetail(data.data))
@@ -42,18 +40,31 @@ const loadPhonesByName = phoneId => {
 const displayPhoneDetail = phone => {
     console.log(phone);
     const phoneDetails = document.getElementById('phone-details');
+    const divimg = document.createElement('div');
+    // divimg.classList.add('');
+    divimg.innerHTML = `<img src="${phone.image}" class="card-img-top w-50 mx-auto" alt="...">`
     const div = document.createElement('div');
-    div.classList.add('card');
+    // div.classList.add('');
+    const h5 = document.createElement('h5');
+    h5.innerText = phone.releaseDate;
+    const errorMessage = document.createElement('h6');
+    errorMessage.innerText = 'release Date No found';
+    errorMessage.classList.add('text-danger')
+    if(h5.innerText != ''){
+        divimg.appendChild(h5);
+    }
+    else{
+        divimg.appendChild(errorMessage);
+    }
+    
     div.innerHTML = `
-        <img src="${phone.image}" class="card-img-top" alt="...">
+        
             <div class="card-body">
-              <h5 class="card-title">${phone.releaseDate}</h5>
-              <div class="row row-cols-md-6">
-              </div>
+              
               <p class="card-text"><strong class="fw-semi-bolder">Chipset: </strong>${phone.mainFeatures.chipSet}</p>
               <p class="card-text"><strong class="fw-semi-bolder">Display: </strong>${phone.mainFeatures.displaySize}</p>
               <p class="card-text"><strong class="fw-semi-bolder">Memory: </strong>${phone.mainFeatures.memory}</p>
-              <p class="card-text"><strong class="fw-semi-bolder">Sensors: </strong>${phone.mainFeatures.sensors}</p>
+              <p class="card-text"><strong class="fw-semi-bolder">Sensors:                                      </strong>${phone.mainFeatures.sensors}</p>
               <p class="card-text"><strong class="text-secondary">OTHERS: </strong></p>
               <p class="card-text"><strong class="fw-semi-bolder">Bluetooth: </strong>${phone.others.Bluetooth}</p>
               <p class="card-text"><strong class="fw-semi-bolder">GPS: </strong>${phone.others.GPS}</p>
@@ -63,5 +74,8 @@ const displayPhoneDetail = phone => {
               <p class="card-text"><strong class="fw-semi-bolder">USB: </strong>${phone.others.USB}</p>
             </div>
     `;
+    
+    phoneDetails.appendChild(divimg);
     phoneDetails.appendChild(div);
+    phoneDetails.classList.add('card');
 }
