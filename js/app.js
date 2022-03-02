e3fa1ea6fa80161d10200740dfd2cefe02eb83b0
hasannayeeem <hasannayeeem@gmail.com>
2022-03-02 20:51:10 +0600

    semi-f

--- js/app.js
+++ js/app.js
@@ -48,8 +48,19 @@ const displayPhoneDetail = phone => {
         <img src="${phone.image}" class="card-img-top" alt="...">
             <div class="card-body">
               <h5 class="card-title">${phone.releaseDate}</h5>
-              <p class="card-text">${phone.mainFeatures.sensors}</p>
-              <a href="#" class="btn btn-primary">Go somewhere</a>
+              <div class="row row-cols-md-6">
+              </div>
+              <p class="card-text"><strong class="fw-semi-bolder">Chipset: </strong>${phone.mainFeatures.chipSet}</p>
+              <p class="card-text"><strong class="fw-semi-bolder">Display: </strong>${phone.mainFeatures.displaySize}</p>
+              <p class="card-text"><strong class="fw-semi-bolder">Memory: </strong>${phone.mainFeatures.memory}</p>
+              <p class="card-text"><strong class="fw-semi-bolder">Sensors: </strong>${phone.mainFeatures.sensors}</p>
+              <p class="card-text"><strong class="text-secondary">OTHERS: </strong></p>
+              <p class="card-text"><strong class="fw-semi-bolder">Bluetooth: </strong>${phone.others.Bluetooth}</p>
+              <p class="card-text"><strong class="fw-semi-bolder">GPS: </strong>${phone.others.GPS}</p>
+              <p class="card-text"><strong class="fw-semi-bolder">NFC: </strong>${phone.others.NFC}</p>
+              <p class="card-text"><strong class="fw-semi-bolder">Radio: </strong>${phone.others.Radio}</p>
+              <p class="card-text"><strong class="fw-semi-bolder">WLAN: </strong>${phone.others.WLAN}</p>
+              <p class="card-text"><strong class="fw-semi-bolder">USB: </strong>${phone.others.USB}</p>
             </div>
     `;
     phoneDetails.appendChild(div);
